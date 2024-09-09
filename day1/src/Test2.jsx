c4d05d85bf19f32e3b8ff24b6f940b901522c604
choi14123 <ckdghks5565@gmail.com>
2024-09-09 15:18:47 +0900

    closed #3, #4

--- day1/src/Test2.jsx
+++ day1/src/Test2.jsx
@@ -1,10 +1,18 @@
+import { useState } from 'react';
+
 const Test2 = () => {
-  
-
-  return <div>
-    <input name="text"></input>
-  </div>
+  const [state, setName] = useState({name: "최창환", log: [] });
+  const onChangeHandler = (e) => {
+    const { name, value } = e.target;
+    if (name === "name") return setName({ ...state, [name]: value, log: [...state.log, value] })
+      setName({ ...state, [name]: value })
+  }
+  return (
+    <div className="App">
+      <input name="name" onChange={onChangeHandler} value={state.name} />
+      {state.log.map((value) => <div> {value}</div>)}
+    </div>
+  );
 }
-
 
 export default Test2;
