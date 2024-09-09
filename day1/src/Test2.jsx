6e122cb2c3f1a9312b3f5407ff33b48d9a679261
choi14123 <ckdghks5565@gmail.com>
2024-09-09 14:46:03 +0900

    close #3

---
+++ day1/src/Test2.jsx
@@ -0,0 +1,10 @@
+const Test2 = () => {
+  
+
+  return <div>
+    <input name="text"></input>
+  </div>
+}
+
+
+export default Test2;
--- day1/src/index.js
+++ day1/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Test2 from './Test2';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Test2 />
 );
 
 // If you want to start measuring performance in your app, pass a function
